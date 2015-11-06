dc5bb716a1fd06a4ba97eacde66229981045bc8f
Kevin Townsend <kt@krtownsend.com>
2015-11-06 14:41:08 -0600

    .

---
+++ caeCnySpmv/appCnySpmv/cpCnySpmv.s
@@ -0,0 +1,22 @@
+#TODO: add header information
+	.file	"cpCnySpmv.s"
+	.ctext
+
+#TODO: remove sample function
+# sample fuction
+cpTalk:
+    .globl  cpTalk
+    .type   cpTalk. @function
+    .signature pdk=65042
+    caep00 $0
+    rtn
+
+#TODO: add function
+#TODO: name function
+#   .globl functionName
+#   .type functionName. @function
+#   .signature pdk=65042
+#TODO: pass arguments using mov
+#TODO: call custom instruction
+#TODO: return value using mov
+#   rtn
