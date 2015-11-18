28bf5e3cc8172c32a1cd4fb0451411ba01b486b4
Kevin Townsend <kt@krtownsend.com>
2015-11-17 22:22:00 -0600

    .

--- rtl/pe/spmv_pe_tb.v
+++ rtl/pe/spmv_pe_tb.v
@@ -69,8 +69,8 @@ struct SmacHeader{
     ull r2[8];
 };
 */
-    initial $readmemh("cant0.hex", mock_main_memory);
-    //initial $readmemh("example.hex", mock_main_memory);
+    //initial $readmemh("cant0.hex", mock_main_memory);
+    initial $readmemh("example.hex", mock_main_memory);
     wire [63:0] width = mock_main_memory[1];
     wire [63:0] height = mock_main_memory[2];
     wire [63:0] nnz = mock_main_memory[3];
